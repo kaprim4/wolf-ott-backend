439292a3a87f7b3dba219fdeeca8b3c7f7a70e74
YAHYA Ali <87yahya@gmail.com>
2024-08-20 19:36:45 +0100

    Add missing @EnableDiscoveryClient to some services

--- user-service/src/main/java/com/wolfott/mangement/user/configs/MapperConfig.java
+++ user-service/src/main/java/com/wolfott/mangement/user/configs/MapperConfig.java
@@ -1,9 +1,5 @@
 package com.wolfott.mangement.user.configs;
 
-import com.wolfott.mangement.user.repositories.GroupRepository;
-import com.wolfott.mangement.user.repositories.PackageRepository;
-import com.wolfott.mangement.user.repositories.ProfileRepository;
-import com.wolfott.mangement.user.repositories.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,8 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MapperConfig {
     @Bean
-    public ModelMapper modelMapper(UserRepository userRepository, GroupRepository groupRepository, PackageRepository packageRepository, ProfileRepository profileRepository) {
-        ModelMapper modelMapper = new ModelMapper();
-        return modelMapper;
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 }
