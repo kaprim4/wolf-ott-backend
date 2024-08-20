03cf905b3970026ddb349f665b394ff7b6dd7964
YAHYA Ali <87yahya@gmail.com>
2024-08-20 11:47:09 +0100

    Update WebConfig of Gateway-service

--- gateway-service/src/main/java/com/wolfott/gateway/config/WebConfig.java
+++ gateway-service/src/main/java/com/wolfott/gateway/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig extends org.springframework.web.cors.CorsConfiguration {
     @Bean
     public CorsWebFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("http://localhost:4200");
+        corsConfiguration.addAllowedOrigin("*");
         corsConfiguration.addAllowedMethod("*");
         corsConfiguration.addAllowedHeader("*");
         corsConfiguration.setMaxAge(9999999L);
