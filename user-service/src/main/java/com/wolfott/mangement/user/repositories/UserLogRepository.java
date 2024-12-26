7ff5e81e021432835fb9b17c6931f630e399f08d
kaprim4 <87yahya@gmail.com>
2024-12-26 23:04:40 +0100

    Add line update by preset sort updates

--- user-service/src/main/java/com/wolfott/mangement/user/repositories/UserLogRepository.java
+++ user-service/src/main/java/com/wolfott/mangement/user/repositories/UserLogRepository.java
@@ -4,8 +4,10 @@ import com.wolfott.mangement.user.models.UserLog;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserLogRepository extends JpaRepository<UserLog, Long> {
 
+    @Query("SELECT l FROM UserLog l WHERE l.owner=:owner ORDER BY l.id DESC")
     Page<UserLog> findAllByOwner(Long owner, Pageable pageable);
 }
--- user-service/src/main/java/com/wolfott/mangement/user/services/UserLogService.java
+++ user-service/src/main/java/com/wolfott/mangement/user/services/UserLogService.java
@@ -3,7 +3,9 @@ package com.wolfott.mangement.user.services;
 import com.wolfott.mangement.user.responses.UserLogCompactResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserLogService {
+
     Page<UserLogCompactResponse> getAll(Pageable pageable);
 }
